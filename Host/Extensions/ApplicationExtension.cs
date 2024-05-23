b2a877df40384579077eeffc22ab776e6d95aef1
Yusuf Adeyemi <yusfate81@gmail.com>
2024-05-23 19:08:30 +0100

    exception handler

---
+++ Host/Extensions/ApplicationExtension.cs
@@ -0,0 +1,12 @@
+﻿using WebApi.Middlewares;
+
+namespace WebApi.Extensions
+{
+    public static class ApplicationExtension
+    {
+        public static void UseExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandler>();
+        }
+    }
+}
