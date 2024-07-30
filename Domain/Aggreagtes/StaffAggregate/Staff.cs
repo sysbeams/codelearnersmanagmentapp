2ab83f5387d8d1abddefe2161efb249cb5b16df4
Christy4ll <ogbodopatrick199@gmail.com>
2024-07-30 14:08:25 +0100

    Correction from Admin to Staff Entity implemented

--- Domain/Aggreagtes/StaffAggregate/Staff.cs
+++ Domain/Aggreagtes/StaffAggregate/Staff.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Aggreagtes.AdminAggregate
+namespace Domain.Aggreagtes.StaffAggregate
 {
     public class Staff : AuditableEntity, IAggregateRoot
     {
