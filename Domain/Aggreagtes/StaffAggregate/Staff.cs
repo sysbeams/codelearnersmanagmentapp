b2e376f481506077a5492b92c278e011641d277d
Lawal <141651904+lawsontechnology@users.noreply.github.com>
2024-07-31 12:58:47 +0100

    Update On Staff Entities.cs

--- Domain/Aggreagtes/StaffAggregate/Staff.cs
+++ Domain/Aggreagtes/StaffAggregate/Staff.cs
@@ -4,44 +4,34 @@ using Domain.Aggreagtes.StudentAggregate;
 using Domain.Aggreagtes.UserAggregate;
 using Domain.Common.Contracts;
 using Domain.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Aggreagtes.StaffAggregate
 {
-    public class Staff : AuditableEntity , IAggregateRoot
+    public class Staff : AuditableEntity, IAggregateRoot
     {
-        public string StaffNumber { get; private set; }
-        public string Lastname { get; private set; } = default!;
         public string Firstname { get; private set; } = default!;
-        public string PhoneNumber { get; private set; }
+        public string Lastname { get; private set; } = default!;
         public string EmailAddress { get; private set; }
-        public DateOnly? DateOfBirth { get; private set; }
-        public Address? Address { get; private set; }
-        public ICollection<Enrollment> Enrollments { get; private set; } = new HashSet<Enrollment>();
-        public ICollection<Course> Courses { get; private set; } = new HashSet<Course>();
+        public string Phonenumber { get; private set; } = default!;
         public Guid? UserId { get; private set; } = default!;
         public virtual User? User { get; private set; }
-
         public string Fullname => $"{Firstname}{Lastname}";
-        internal Staff(string staffNumber, string firstname, string lastname, string phoneNumber, string emailAddress)
+        public Address? Address { get; private set; }
+        public ICollection<Course> Courses { get; private set; } = new HashSet<Course>();
+
+        internal Staff(string firstname, string lastname, string emailAddress, string phonenumber)
         {
-            StaffNumber = staffNumber;
             Firstname = firstname;
             Lastname = lastname;
-            PhoneNumber = phoneNumber;
             EmailAddress = emailAddress;
-
+            Phonenumber = phonenumber;
         }
+
         public void AddAddress(string street, string city, string state, string country)
         {
             if (Address != null)
                 throw new InvalidAddressUpdateException($"The staff {Fullname} has address. Try update");
             Address = new Address(street, city, state, country);
         }
-       
     }
 }
