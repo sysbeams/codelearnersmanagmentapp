840604ef4d8e32ba3377b5239d37306448a56ab5
Christy4ll <ogbodopatrick199@gmail.com>
2024-08-16 19:05:00 +0100

    Email Integration Using Brevo Email Provider

---
+++ Application/EmailSender/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Application.Emails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.EmailSender
+{
+    public interface IEmailSender 
+    {
+        void SendEmail(Email email);
+    }
+}
