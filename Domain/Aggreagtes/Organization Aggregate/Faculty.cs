dcbb513b4ef77a8441ce965c18a520160b7f8953
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-10-04 10:09:00 +0100

    Creating Organization Aggregate

---
+++ Domain/Aggreagtes/Organization Aggregate/Faculty.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggreagtes.Organization_Aggregate
+{
+    public class Faculty : AuditableEntity<Guid>
+    {
+        public required string Name { get; set; } 
+        public Guid StaffId { get; set; }
+        public required Organization Organization { get; set; }
+    }
+}
---
+++ Domain/Aggreagtes/Organization Aggregate/Organization.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggreagtes.Organization_Aggregate
+{
+    public class Organization : AuditableEntity, IAggregateRoot
+    {
+        public required string Name { get; set; } = default!;
+
+    }
+}
