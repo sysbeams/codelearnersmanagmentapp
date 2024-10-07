31c8dfeef70d343e6efe3f1f63c1e1dc9a3adae0
nuhu ibrahim <nuhuibrahim04@gmail.com>
2024-10-07 14:32:23 +0100

    Implement-Class-Aggregrate 1.0

--- Domain/Aggreagtes/ClassAggregate/Assignment.cs
+++ Domain/Aggreagtes/ClassAggregate/Assignment.cs
@@ -15,16 +15,18 @@ namespace Domain.Aggreagtes.ClassAggregate
         public Class Class { get; private set; } 
         public DateTime SubmissionDate { get; private set; } = default!;
         public decimal Grade { get; private set; } = default!;
+        public string Content { get; private set; }
 
         #region Constructor
         private Assignment () { }
-        public Assignment(int courseId, int batchId, Class classEntity, DateTime submissionDate, decimal grade)
+        public Assignment(int courseId, int batchId, Class classEntity, DateTime submissionDate, decimal grade, string content)
         {
             CourseId = courseId;
             BatchId = batchId;
             Class = classEntity;
             SubmissionDate = submissionDate;
             Grade = grade;
+            Content = content;
         }
         #endregion
     }
