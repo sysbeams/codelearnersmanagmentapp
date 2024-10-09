f7421e616ffa790e742438bf5913397c68b2b2fd
hp <ebukaanthony9@gmail.com>
2024-10-09 01:09:43 +0100

    feature/111-main-Implement-Applicant-Aggregate

---
+++ Domain/Aggreagtes/ApplicantAggregate/Application.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Aggreagtes.CourseAggregate;
+using Domain.Common.Contracts;
+
+namespace Domain.Aggreagtes.ApplicantAggregate
+{
+    public class Application : AuditableEntity
+    {
+        public Guid BatchId { get; private set; }
+        public Guid CourseId { get; private set; }
+        public Applicant Applicant { get; private set; }
+        public Assessment Assessment { get; private set; }
+        public CourseMode CourseMode { get; private set; }
+        public ApplicationStatus ApplicationStatus { get; private set; }
+
+        public Application(Applicant applicant, Guid batchId, Guid courseId, Assessment assessment, CourseMode courseMode, ApplicationStatus applicationStatus)
+        {
+            Applicant = applicant;
+            BatchId = batchId;
+            CourseId = courseId;
+            Assessment = assessment;
+            CourseMode = courseMode;
+            ApplicationStatus = applicationStatus;
+        }
+    }
+}
