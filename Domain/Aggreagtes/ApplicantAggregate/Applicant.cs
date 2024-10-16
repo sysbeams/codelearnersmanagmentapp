d1ce589fb72056f07b0df9e9bcfcc15cee216efb
Kajol232 <ridamot123@yahoo.com>
2024-10-16 11:40:24 +0100

    remove applicant aggregrate

--- Domain/Aggreagtes/ApplicantAggregate/Applicant.cs
+++ Domain/Aggreagtes/ApplicantAggregate/Applicant.cs
@@ -1,35 +1,27 @@
-﻿using Domain.Aggreagtes.StudentAggregate;
+﻿using Domain.Aggreagtes.UserAggregate;
 using Domain.Common.Contracts;
 
 namespace Domain.Aggreagtes.ApplicantAggregate;
 
 public class Applicant : AuditableEntity, IAggregateRoot
 {
-    public string FirstName { get; private set; } = default!;
     public string LastName { get; private set; } = default!;
-    public string MiddleName { get; private set; } = default!;
+    public string FirstName { get; private set; } = default!;
     public string EmailAddress { get; private set; } = default!;
-    public string PhoneNumber { get; private set; }
-    public DateOnly? DateOfBirth { get; private set; }
-    public Gender Gender { get; private set; } = default!;
-    public NextOfKin NextOfKin { get; private set; } 
-    public Address Address { get; private set; } = default!;
-    public ICollection<Application> Applications = new HashSet<Application>();
+    public Guid? UserId { get; private set; } = default!;
+    public virtual User? User { get; private set; }
+    public string Fullname => $"{FirstName} {LastName}";
 
     #region Constructor
     private Applicant() { }
 
-    public Applicant(string firstName, string lastName, string middleName, string emailAddress, string phoneNumber, DateOnly dateOfBirth, Gender gender, NextOfKin nextOfKin, Address address)
+    public Applicant(string firstname, string lastname, string emailAddress, Guid userId)
     {
-        FirstName = firstName;
-        LastName = lastName;
-        MiddleName = middleName;
+        FirstName = firstname;
+        LastName = lastname;
         EmailAddress = emailAddress;
-        PhoneNumber = phoneNumber;
-        DateOfBirth = dateOfBirth;
-        Gender = gender;
-        NextOfKin = nextOfKin;
-        Address = address;
+        UserId = userId;
     }
     #endregion
 }
+
--- Infrastructure/Persistence/Context/ApplicationContext.cs
+++ Infrastructure/Persistence/Context/ApplicationContext.cs
@@ -1,5 +1,6 @@
 ﻿
 using Domain.Aggreagtes.ApplicantAggregate;
+using Domain.Aggreagtes.Assessment_Aggregate;
 using Domain.Aggreagtes.CourseAggregate;
 using Domain.Aggreagtes.EnrollmentAggregate;
 using Domain.Aggreagtes.LectureAggregate;
