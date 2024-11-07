83f4356339e31ecbb914d5c2ab128ecde4e2b691
Esther <otufaleesther@gmail.com>
2024-11-07 14:02:21 +0100

    feature #153 commit
    Related Work Items: #1

---
+++ Infrastructure/Persistence/Context/EntityTypeConfiguration/EmploymentContractEntityTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Aggreagtes.Organization_Aggregate;
+using Domain.Aggreagtes.StaffAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Context.EntityTypeConfiguration
+{
+    public class EmploymentContractEntityTypeConfiguration : IEntityTypeConfiguration<EmploymentContract>
+    {
+        public void Configure(EntityTypeBuilder<EmploymentContract> builder)
+        {
+            builder.HasKey(x => x.ContractId);
+            builder.HasOne<Staff>()
+                .WithMany(x => x.EmploymentContracts)
+                .HasForeignKey(x => x.StaffId);
+            builder.HasOne<Organization>()
+                .WithOne()
+                .HasForeignKey("OrganizationId");
+            builder.Property(x => x.Salary)
+                .IsRequired();
+            builder.Property(x => x.Benefits)
+                .IsRequired();
+            builder.Property(x => x.OrganizationId)
+                .IsRequired();
+            builder.Property(x => x.StartDate)
+                .IsRequired();
+
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Context/EntityTypeConfiguration/StaffEntityTypeConfiguration.cs
@@ -0,0 +1,36 @@
+
+
+using Domain.Aggreagtes.StaffAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class StaffEntityTypeConfiguration : IEntityTypeConfiguration<Staff>
+{
+    public void Configure(EntityTypeBuilder<Staff> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.StaffNo)
+            .IsRequired();
+        builder.Property(x => x.Firstname)
+            .IsRequired();
+        builder.Property(x => x.EmailAddress)
+            .IsRequired();
+        builder.Property(x => x.Phonenumber)
+            .IsRequired();
+        builder.Property(x => x.Gender)
+            .IsRequired();
+        builder.Property(x => x.DateOfBirth)
+            .IsRequired();
+        builder.HasOne(x => x.ContactInfo)
+            .WithOne();
+        builder.HasOne(x => x.NextOfKin)
+           .WithOne();
+        builder.HasOne(x => x.BankDetails)
+           .WithOne();
+        builder.HasMany(x => x.AdjunctDepartments)
+            .WithOne();
+        builder.HasMany(x => x.EmploymentContracts)
+            .WithOne();
+
+    }
+}
