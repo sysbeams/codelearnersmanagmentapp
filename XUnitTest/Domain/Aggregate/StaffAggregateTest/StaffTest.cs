fd2990cb9dfa8130d562ea8129e139ecd6ea7f19
Esther <otufaleesther@gmail.com>
2024-11-07 18:41:44 +0100

    fifth commit

--- XUnitTest/Domain/Aggregate/StaffAggregateTest/StaffTest.cs
+++ XUnitTest/Domain/Aggregate/StaffAggregateTest/StaffTest.cs
@@ -1,11 +1,40 @@
 ﻿using Domain.Aggreagtes.Organization_Aggregate;
 using Domain.Aggreagtes.StaffAggregate;
 using Domain.Enums;
+using Domain.ValueObjects;
 
 namespace XUnitTest.DomainTest.StaffAggregateTest
 {
     public class StaffTest
     {
+        private ContactInformation CreateContactInformation()
+        {
+                 var address = Address.CreateAddress(
+                 streetNo: 123,
+                 streetName: "Gbonogun",
+                 city: "Abeokuta",
+                 state: "Ogun State",
+                 country: "Nigeria"
+                 );
+
+            return  ContactInformation.CreateContactInformation(
+                phoneNumber: "123456789",
+                email: "john@example.com",
+                address: address
+            );
+        }
+
+        private NextOfKinDetails CreateNextOfKin()
+        {
+            var contactInformation = CreateContactInformation();
+
+            return NextOfKinDetails.CreateNextOfKinDetails(
+                nokFirstName: "Jane",
+                nokLastName: "Doe",
+                relationship: "Sister",
+                contactInformation: contactInformation
+            );
+        }
 
         [Fact]
         public void Should_ThrowException_When_StaffNo_IsNull()
@@ -14,8 +43,8 @@ namespace XUnitTest.DomainTest.StaffAggregateTest
 
             var exception = Assert.Throws<ArgumentNullException>(() =>
                 new Staff(staffNo, "John", "Doe", Gender.Male, DateTime.Now, "john@example.com", "123456789",
-                    new ContactInformation("123456789", "john@example.com", "Address"),
-                    new NextOfKin("Jane Doe", "Sister", "123456789"),
+                    CreateContactInformation(),
+                    CreateNextOfKin(),
                     new BankDetails("Bank", "1234567890", "Branch"), null
                 )
             );
@@ -31,8 +60,8 @@ namespace XUnitTest.DomainTest.StaffAggregateTest
 
             var exception = Assert.Throws<ArgumentNullException>(() =>
                 new Staff("123", firstname, "Doe", Gender.Male, DateTime.Now, "john@example.com", "123456789",
-                          new ContactInformation("123456789", "john@example.com", "Address"),
-                          new NextOfKin("Jane Doe", "Sister", "123456789"),
+                          CreateContactInformation(),
+                          CreateNextOfKin(),
                           new BankDetails("Bank", "1234567890", "Branch"),null)
             );
 
@@ -45,8 +74,8 @@ namespace XUnitTest.DomainTest.StaffAggregateTest
 
             var exception = Assert.Throws<ArgumentNullException>(() =>
                 new Staff("123", "John", "Doe", Gender.Male, DateTime.Now, "john@example.com", "123456789",
-                          new ContactInformation("123456789", "john@example.com", "Address"),
-                          new NextOfKin("Jane Doe", "Sister", "123456789"),
+                          CreateContactInformation(),
+                          CreateNextOfKin(),
                           new BankDetails("Bank", "1234567890", "Branch"),
                           null)
             );
