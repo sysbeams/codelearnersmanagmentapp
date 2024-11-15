a120e63be56fb82cf7211ac7fac999305380e2a5
Lawal Rofeeq <163846014+AbdurRofeeq@users.noreply.github.com>
2024-11-15 04:34:18 +0100

    Update Department.cs

--- Domain/Aggreagtes/Organization Aggregate/Department.cs
+++ Domain/Aggreagtes/Organization Aggregate/Department.cs
@@ -49,22 +49,22 @@ public class Department : AuditableEntity<Guid>
 
     #region Helper Method
 
-    public void AddAdjunctStaff(AdjuncStaff adjuncStaff)
+    public void AddAdjunctStaff(AdjunctStaff adjunctStaff)
     {
-        if (adjuncStaff == null)
-            throw new ArgumentNullException(nameof(adjuncStaff), "AdjuncStaff cannot be null.");
+        if (adjunctStaff == null)
+            throw new ArgumentNullException(nameof(adjunctStaff), "AdjunctStaff cannot be null.");
 
-        if (!AdjuncStaffs.Contains(adjuncStaff))
-            AdjuncStaffs.Add(adjuncStaff);
+        if (!AdjunctStaffs.Contains(adjunctStaff))
+            AdjunctStaffs.Add(adjunctStaff);
     }
 
-    public void RemoveAdjunctStaff(AdjuncStaff adjuncStaff)
+    public void RemoveAdjunctStaff(AdjunctStaff adjunctStaff)
     {
-        if (adjuncStaff == null)
-            throw new ArgumentNullException(nameof(adjuncStaff), "AdjuncStaff cannot be null.");
+        if (adjunctStaff == null)
+            throw new ArgumentNullException(nameof(adjunctStaff), "AdjunctStaff cannot be null.");
 
-        if (AdjuncStaffs.Contains(adjuncStaff))
-            AdjuncStaffs.Remove(adjuncStaff);
+        if (AdjunctStaffs.Contains(adjunctStaff))
+            AdjunctStaffs.Remove(adjunctStaff);
     }
     #endregion
 }
